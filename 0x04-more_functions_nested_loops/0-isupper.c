b4daebccb979b3b92830909452c3d4b9c3a8438f
Ngwu Jerome Nnaemeka <ngwujerome2000@gmail.com>
2022-09-28 11:18:10 -0700

    corrections

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case letter
+ * @c : character to check the case
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+
+}
